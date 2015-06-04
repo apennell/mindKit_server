e5059cccfdb1f78063583966817e39c54e6503f2
Annie Pennell <annie@anniepennell.com>
2015-06-03 18:42:10 -0700

    all mvp form fields working

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/logs', function(req, res) {
   var thoughts = req.body;
   console.log(thoughts);
   console.log(req.body);
-  db.ThoughtLog.create({situation: thoughts.situation, emotion1: thoughts.emotion1, emotion2: thoughts.emotion2, emotion3: thoughts.emotion3, emotion4: thoughts.emotion4, emotion5: thoughts.emotion5}, function(err, thought) {
+  db.ThoughtLog.create({situation: thoughts.situation, emotion1: thoughts.emotion1, emotion2: thoughts.emotion2, emotion3: thoughts.emotion3, emotion4: thoughts.emotion4, emotion5: thoughts.emotion5, emotion1AmountA: thoughts.emotion1AmountA, emotion2AmountA: thoughts.emotion2AmountA, emotion3AmountA: thoughts.emotion3AmountA, emotion4AmountA: thoughts.emotion4AmountA, emotion5AmountA: thoughts.emotion5AmountA, thought: thoughts.thought, altThought: thoughts.altThought}, function(err, thought) {
     console.log(thought);
   });
 });
